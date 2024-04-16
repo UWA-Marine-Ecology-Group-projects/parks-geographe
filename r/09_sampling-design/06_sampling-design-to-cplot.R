7771ca2e3d691207a7ccb18d69516e0921cd9a6c
claudehspencer <claudespencer111@gmail.com>
2024-04-17 05:49:53 +0800

    added mrk file for new preferential points

--- r/09_sampling-design/06_sampling-design-to-cplot.R
+++ r/09_sampling-design/06_sampling-design-to-cplot.R
@@ -28,6 +28,10 @@ pot <- read.csv("output/mbh-design/craypot_sampling-design_geographe-march.csv")
   dplyr::mutate(siteID = str_replace_all(siteID, "GB-CP-", "POT-")) %>%
   glimpse()
 
+bruv2 <- read.csv("output/sampling-design/bruv_in-out_sampling-design_geographe-april.csv") %>%
+  dplyr::mutate(siteID = str_replace_all(siteID, "GB-BV-", "BRUV-P")) %>%
+  glimpse()
+
 # BRUVs
 cplot.bruv <- data.frame("mark" = c("mark"),
                              "PXYCSLM" = c("PXYCSLM"),
@@ -62,6 +66,38 @@ write.table(cplot.bruv , "output/mbh-design/bruv_sampling-design_geographe-march
 paste(min(cplot.bruv$lat), min(cplot.bruv$lon),
       max(cplot.bruv$lat),max(cplot.bruv$lon), sep = ",") # Add into 'Bounds' - not sure its needed or not 
 
+cplot.bruv2 <- data.frame("mark" = c("mark"),
+                         "PXYCSLM" = c("PXYCSLM"),
+                         lon = measurements::conv_unit(bruv2$lon_WGS84, 
+                                                       from = "dec_deg", 
+                                                       to = "deg_dec_min"),
+                         lat = measurements::conv_unit(bruv2$lat_WGS84, 
+                                                       from = "dec_deg", 
+                                                       to = "deg_dec_min"),
+                         "symbol" = c("Green Star"),
+                         "ptcode" = bruv2$siteID,
+                         c(0)) %>%
+  separate(lon, into = c("lon.hour", "lon.dm"), sep = " ") %>%
+  separate(lat, into = c("lat.hour", "lat.dm"), sep = " ") %>%
+  separate(lon.dm, into = c("lon.min", "lon.dec"), sep = "\\.") %>%
+  separate(lat.dm, into = c("lat.min", "lat.dec"), sep = "\\.") %>%
+  dplyr::mutate(lon.dec = str_trunc(.$lon.dec, width = 4,  side = "right", ellipsis = ""),
+                lon.min = str_pad(.$lon.min, side = "left", pad = "0", width = 2),
+                lat.dec = str_trunc(.$lat.dec, width = 4,  side = "right", ellipsis = ""),
+                lat.min = str_pad(.$lat.min, side = "left", pad = "0", width = 2)) %>%
+  dplyr::mutate(lon.dec = str_trunc(.$lon.dec, width = 4,  side = "right", ellipsis = ""),
+                lon.dec = str_pad(.$lon.dec, side = "right", pad = "0", width = 4)) %>%
+  dplyr::mutate(lon = paste0(paste(lon.hour, lon.min, lon.dec, sep = "."), "E"),
+                lat = sub('.', '',paste0(paste(lat.hour, lat.min, lat.dec, sep = "."), "S"))) %>%
+  dplyr::select(mark, PXYCSLM, lon, lat, symbol, ptcode, c.0.) %>%
+  glimpse()
+
+head(cplot.bruv2)
+write.table(cplot.bruv2 , "output/sampling-design/bruv2_sampling-design_geographe-april.txt", sep = " ", 
+            col.names = FALSE, row.names = FALSE, quote = FALSE)
+
+paste(min(cplot.bruv2$lat), min(cplot.bruv2$lon),
+      max(cplot.bruv2$lat),max(cplot.bruv2$lon), sep = ",") # Add into 'Bounds' - not sure its needed or not 
 # BOSS
 cplot.boss <- data.frame("mark" = c("mark"),
                          "PXYCSLM" = c("PXYCSLM"),
@@ -143,7 +179,8 @@ paste(min(cplot.pot$lat), min(cplot.pot$lon),
 # <EOH>
 
 # then run this to duplicate each file and convert the copy to .MRK file
-txts <- list.files("output/mbh-design/", "*.txt", full.names = T)
+txts <- list.files("output/sampling-design/", "*.txt", full.names = T)
+txts <- "output/sampling-design/bruv_preferential_sampling-design_geographe-april.txt"
 for(filei in txts){
   file.copy(filei, paste(gsub(".txt", ".MRK", filei)))
 }
